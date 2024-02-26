6ca0aca14aab9dd859ab1b16bb1a71357df4fcae
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-02-26 20:57:47 +0100

    User import

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import datetime
-from django.conf import settings
-# from django.contrib.auth.models import User
+# from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,5 +11,4 @@ class Todo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
     created_at = models.DateField(default=datetime.date.today)
-    user = models.ForeignKey( settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,) #User
+    user = models.ForeignKey( User, on_delete=models.CASCADE,) 
