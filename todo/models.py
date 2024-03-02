1932dfe2520276f727815affae3c94ce3a1452fd
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-03-02 17:05:39 +0100

    model function to serializers

--- todo/models.py
+++ todo/models.py
@@ -11,4 +11,11 @@ class Todo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
     created_at = models.DateField(default=datetime.date.today)
-    user = models.ForeignKey( User, on_delete=models.CASCADE,) 
+    user = models.ForeignKey( User, on_delete=models.CASCADE,default=None) 
+    
+    def time_passed(self):
+        today = datetime.date.today()
+        delta = today - self.created_at
+        return delta.days
+        
+
--- todo/serializers.py
+++ todo/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from .models import Todo
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+     class Meta:
+        model = User
+        fields = ['id','first_name','last_name','username','email']
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    # user = UserSerializer()
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
     class Meta:
         model = Todo
-        fields = ['id','title', 'description', 'created_at']
+        fields = ['id','title', 'description', 'created_at','user', 'time_passed']
