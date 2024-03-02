2240b2c5a6ee8b5653d9e606a94bc8e7e8d044f9
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-03-02 20:32:20 +0100

    create function

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,15 +1,21 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Todo
-
+from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
+
+  class Meta:
         model = User
-        fields = ['id','first_name','last_name','username','email']
+        # user = request.user
+        fields = ['id', 'first_name', 'last_name', 'username', 'email',] 
+
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
-    # user = UserSerializer()
+    # user = CurrentUserDefault()
     user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+    
     class Meta:
         model = Todo
-        fields = ['id','title', 'description', 'created_at','user', 'time_passed']
+        # user = request.user
+        fields = ['id', 'title', 'description', 'created_at', 'user', 'time_passed']
